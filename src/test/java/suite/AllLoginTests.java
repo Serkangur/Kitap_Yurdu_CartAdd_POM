d16f379112d1507efb3c0af03b5523348402d3ef
serkan <serkangur002@gmail.com>
2025-01-06 06:50:30 +0300

    Update AllLoginTests.java

--- src/test/java/suite/AllLoginTests.java
+++ src/test/java/suite/AllLoginTests.java
@@ -1,4 +1,23 @@
+
 package suite;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CartAdd_001.class,
+        CartAdd_002.class,
+        CartAdd_003.class,
+        CartAdd_004.class,
+        CartAdd_005.class,
+        CartAdd_006.class,
+        CartAdd_007.class,
+        CartAdd_008.class,
+
+
+})
 public class AllLoginTests {
+
 }
